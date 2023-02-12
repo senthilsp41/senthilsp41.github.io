049c109360d3170381208baba4bcc079d246b503
Senthil <81291227+senthilsp41@users.noreply.github.com>
2023-02-12 14:50:07 +0530

    Add files via upload

---
+++ js course/index.js
@@ -0,0 +1,25 @@
+.Btn.Btn-profile{
+    Position: Relative;
+Display: Inline-Block;
+Margin: 10px;
+margin-top:20px;
+Padding: 15px 30px;
+Text-Align: right;
+Font-Size: 16px;
+Letter-Spacing: 1px;
+Text-Decoration: None;
+Color: #ffffff;
+Background: Mediumseagreen;
+Border: None;
+Border-Radius: 50px;
+Cursor: Pointer;
+Transition: Ease-Out 0.5s;
+    align-items: center;
+
+}
+.Btn.Btn-profile:{
+    box-shadow: inset 0 0 0 0 #303439;
+}
+.Btn.Btn-profile:hover{
+    box-shadow: inset 0 0 0 25px #303439;
+}
